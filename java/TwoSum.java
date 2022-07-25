da7825744123f6eca529d6b16d8e97f1514bd181
Carter Chan-Nui <26908609+carterchannui@users.noreply.github.com>
2022-07-25 11:45:20 -0700

    implemented O(n) solution for TwoSum

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // Brute Force O(n^2):
+        /*
         int[] indices = new int[2];
-        
         // This implementation does not double count.
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -13,5 +14,23 @@ class Solution {
             }
         }
         return indices;
+        */
+
+        // Two Pass O(n):
+        HashMap<Integer, Integer> vals = new HashMap<>();
+        // Add items to hashmap.
+        for (int i = 0; i < nums.length; i++) { 
+            vals.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int[] result = new int[2];
+            // Ensure that the complement is not nums[i] itself.
+            if (vals.containsKey(target - nums[i]) && vals.get(target - nums[i]) != i) {
+                result[0] = i;
+                result[1] = vals.get(target - nums[i]);
+                return result;
+            }
+        }
+        return null;
     }
 }
